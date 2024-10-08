cceda7eaa37c1651b3194422c3d588c4a8b98c11
zb656 <145730190+zb656@users.noreply.github.com>
2024-10-07 21:19:35 -0400

    3, 4, 5

--- 3/sketch.js
+++ 3/sketch.js
@@ -2,12 +2,27 @@
 //       wrap around
 //       bounce
 
+//global/state variable?
+//let x; // outside of all functions bc if it was in setup it would always be true and if it was in draw it would constsantly update to be true
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(20);
-  for (let x = 0; x < width; x += 2) {
-    ellipse(x, height / 2, 100);
-  }
+  // x = 0;
 }
 
-function draw() {}
+function draw() {
+  background(20);
+
+  // draw stuff
+  //modular thingy used to make it reset when it reaches the end
+  let xpos = 5 * frameCount;
+  ellipse(xpos % width, height / 2, 100);
+
+  //update some parameter
+  // x = x + 5;
+
+  // //check if need to reset x
+  // if (x > width) {
+  //   x = 0;
+  // }
+}
--- 4/sketch.js
+++ 4/sketch.js
@@ -2,9 +2,26 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(20);
 }
 
 function draw() {
-  ellipse(width / 2, height / 2, 20);
+  background(20);
+  
+  //period of our cycle
+  let P = 3000;
+  let counter = millis() % P;
+
+  //creates a diameter variable for our ellipse that increases over 2000mls
+  let md = map(counter, 0,P, 10,500);
+  let mg = map(counter, 0, P, 100, 255);
+
+  fill(0, mg, 100);
+  ellipse(width / 2, height / 2, md);
+  //you could draw a girls face with this as bubblegum popping
+  //maybe the circle expands until you click it and then it 'pops'
+  //or it pops at the widest point of the screen if u dont click
+  //some pink on girls face to show bubblegum
+  //omg and you can make it lighter pink over time
+
+  //or you could make a lot of strobing lights lol
 }
